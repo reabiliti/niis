72d1aa5e4014bd4ef1ba23e20c75abc5fc0e61c5
Your Name <you@example.com>
2018-01-23 16:51:35 +0300

    Complete Certificate controller

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -1,12 +1,43 @@
 class CertificatesController < ApplicationController
 
+  def index
+    @certificates = Certificate.all
+  end
+
+  def show
+    @certificate = certificate_find
+  end
+
   def new
+    @certificate = Certificate.new
+  end
+
+  def edit
+    @certificate = certificate_find
   end
 
   def create
     @certificate = Certificate.new(certificate_params)
-    @certificate.save
-    redirect_to @certificate
+    if @certificate.save
+      redirect_to @certificate
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @certificate = certificate_find
+    if @certificate.update(certificate_params)
+      redirect_to @certificate
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @certificate = certificate_find
+    @certificate.destroy
+    redirect_to certificates_path
   end
 
   private
@@ -14,7 +45,11 @@ class CertificatesController < ApplicationController
     def certificate_params
       params.require(:certificate).permit(:cer_number, :cer_blank_number, 
                                           :cer_validity_from, :cer_validity_to, 
-                                          :cer_org_registration))
+                                          :cer_org_registration)
+    end
+
+    def certificate_find
+      Certificate.find(params[:id])
     end
 
 end
--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,2 +1,6 @@
 class Certificate < ApplicationRecord
+
+  validates :cer_blank_number, presence: true,
+                               length: { minimum: 7 }
+
 end
