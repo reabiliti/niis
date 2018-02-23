9f4ad0a11b800523b4fde6344e68b6a50c1e4ea8
Alex <alex.duzh@gmail.com>
2018-02-23 23:08:05 +0300

    correction error sol_date_from

--- db/migrate/20180222120925_change_rename_column_conclusions.rb
+++ db/migrate/20180222120925_change_rename_column_conclusions.rb
@@ -2,6 +2,7 @@ class ChangeRenameColumnConclusions < ActiveRecord::Migration[5.1]
   def change
     change_column :solution_proposals, :solprop_applic_sign, :boolean, null: false
     rename_column :conclusions, :conc_expiry_date, :conc_cert_expiry_date
-    change_column :solutions, :sol_date_from, :datetime
+    remove_column :solutions, :sol_date_from
+    add_column :solutions, :sol_date_from, :datetime
   end
 end
