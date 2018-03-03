07132d7aefa64f688c4f08c717e6d796bfc7c303
Alex <alex.duzh@gmail.com>
2018-03-03 13:25:19 +0300

    03.03.2018 monig

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,6 +2,7 @@ class Proposal < ApplicationRecord
   has_one :solution_proposal, dependent: :destroy, inverse_of: :proposal
   has_one :certcontract, dependent: :destroy
   has_one :inventory, dependent: :destroy
+  validates :prop_number, uniqueness: true
 
   def self.search(search, page)
     if search
