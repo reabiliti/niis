d8a8bcf00dc303643592e4bfe5e490a22d9d0298
Your Name <you@example.com>
2018-03-20 11:26:49 +0300

    test printing

--- app/pdfs/certificate_pdf.rb
+++ app/pdfs/certificate_pdf.rb
@@ -11,30 +11,30 @@ class CertificatePdf < Prawn::Document
 
   def cer_number
     size = 12
-    at_x = set_at_x(59)
+    at_x = set_at_x(46)
 
     at_x_indent = set_at_x(218)
-    at_y = set_at_y(663)
+    at_y = set_at_y(689)
     draw_text @certificate.cert_registration_num, at: [ at_x_indent, at_y ], size: size, style: :bold
 
-    at_x_indent = set_at_x(315)
-    at_y = set_at_y(638)
+    at_x_indent = set_at_x(313)
+    at_y = set_at_y(665)
     draw_text (@certificate.cert_registration_date).strftime("%d.%m.%Y"), at: [ at_x_indent, at_y ], size: size, style: :bold
 
-    at_x_indent = set_at_x(440)
+    at_x_indent = set_at_x(438)
     draw_text (@certificate.cert_expiry_date).strftime("%d.%m.%Y"), at: [ at_x_indent, at_y ], size: size, style: :bold
 
     at_x_indent = set_at_x(230)
-    at_y = set_at_y(600)
+    at_y = set_at_y(625)
     draw_text "рег. № #{@setting.set_os_registration_num}", at: [ at_x_indent, at_y ], size: size, style: :bold
 
-    at_y = set_at_y(595)
+    at_y = set_at_y(620)
     text_box "#{@setting.set_os_name.gsub 'ОРГАН ПО СЕРТИФИКАЦИИ ', ''}\n" +
              "#{@setting.set_os_address}", at: [ at_x, at_y ], size: size, style: :bold, width: 450
 
 
-    at_x_indent = set_at_x(140)
-    at_y = set_at_y(510)
+    at_x_indent = set_at_x(135)
+    at_y = set_at_y(548)
     if @certificate.cert_name_product.split("\r\n").length > 1
       @certificate.cert_name_product.split("\r\n").each_with_index do |cert_name_product, index|
         index == 0 ? (draw_text cert_name_product, at: [ at_x_indent, at_y ], size: size, style: :bold) :
@@ -52,18 +52,18 @@ class CertificatePdf < Prawn::Document
       draw_text @proposal.prop_applic_from_issue, at: [ at_x, at_y ], size: size, style: :bold
     end
 
-    at_x_indent = set_at_x(480)
-    at_y = set_at_y(490)
+    at_x_indent = set_at_x(470)
+    at_y = set_at_y(510)
     draw_text @certificate.cert_code_okp, at: [ at_x_indent, at_y ], size: size, style: :bold
 
-    at_y = set_at_y(410)
+    at_y = set_at_y(420)
     draw_text @certificate.cert_code_tn_ved, at: [ at_x_indent, at_y ], size: size, style: :bold
 
-    at_y = set_at_y(423)
+    at_y = set_at_y(460)
     text_box @certificate.cert_manuf_regulations, at: [ at_x, at_y ], size: size, style: :bold, width: 400
 
-    at_x_indent = set_at_x(159)
-    at_y = set_at_y(350)
+    at_x_indent = set_at_x(158)
+    at_y = set_at_y(372)
     if @certificate.cert_manuf_name.split("\r\n").size > 1
       @certificate.cert_manuf_name.split("\r\n", 2).each_with_index do |cert_manuf_name, index|
         index == 0 ? (draw_text cert_manuf_name, at: [at_x_indent, at_y], size: size, style: :bold) :
@@ -87,8 +87,8 @@ class CertificatePdf < Prawn::Document
                at: [at_x, at_y], size: size, style: :bold, width: 500
     end
 
-    at_x_indent = set_at_x(190)
-    at_y = set_at_y(299)
+    at_x_indent = set_at_x(185)
+    at_y = set_at_y(318)
     if @certificate.cert_applic_name.split("\r\n", 2).size > 1
        @certificate.cert_applic_name.split("\r\n", 2).each_with_index do |prop_applic_name, index|
        index == 0 ? (draw_text prop_applic_name, at: [ at_x_indent, at_y ], size: size, style: :bold) :
@@ -112,16 +112,16 @@ class CertificatePdf < Prawn::Document
                at: [at_x, at_y], size: size, style: :bold, width: 500
     end
 
-    at_x_indent = set_at_x(165)
-    at_y = set_at_y(247)
+    at_x_indent = set_at_x(160)
+    at_y = set_at_y(262)
     @certificate.cert_test_report.split("\r\n", 2).each_with_index do |test_report, index|
       index == 0 ? (draw_text test_report, at: [ at_x_indent, at_y ], size: size, style: :bold) :
                    (text_box test_report, at: [ at_x, at_y ], size: size, style: :bold, width: 500)
       at_y -= 5
     end
 
-    at_x_indent = set_at_x(276)
-    at_y = set_at_y(146)
+    at_x_indent = set_at_x(274)
+    at_y = set_at_y(155)
     draw_text @certificate.cert_add_info, at: [ at_x_indent, at_y ], size: size, style: :bold
 
     at_y -= 12
@@ -132,10 +132,10 @@ class CertificatePdf < Prawn::Document
 
 
     at_x_indent = set_at_x(428)
-    at_y = set_at_y(85)
+    at_y = set_at_y(92)
     draw_text @certificate.cert_chief_org, at: [ at_x_indent, at_y ], size: size, style: :bold
 
-    at_y = set_at_y(60)
+    at_y = set_at_y(64)
     draw_text @certificate.cert_expert, at: [ at_x_indent, at_y ], size: size, style: :bold
 
   end
