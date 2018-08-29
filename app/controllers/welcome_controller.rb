97ad8a84814e88cfc345f1e60cb255eb7716b5f4
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-08-29 03:58:07 +0300

    delete blank files, change bootstrap, change find, refactoring settings

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
+# This controller for search and all proposals
 class WelcomeController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @proposals = Proposal.search(params[:search], params[:page])
+    @proposals = Proposal.search params[:search], params[:page]
     @setting = Setting.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,28 +4,22 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'welcome#index'
-
   resources :settings, except: %i[index destroy]
-  resources :certificates, except: [:index]
-
-  resources :proposals, except: [:index]
-  resources :solution_proposals, except: [:index]
-  resources :protocols, except: [:index]
-  resources :conclusions, except: [:index]
-  resources :solutions, except: [:index]
-  resources :permissions, except: [:index]
-  resources :ikcontracts, except: [:index]
-  resources :certcontracts, except: [:index]
-  resources :inventories, except: [:index]
-  resources :attachments, except: [:index]
+  resources :certificates, except: :index
+  resources :proposals, except: :index
+  resources :solution_proposals, except: :index
+  resources :protocols, except: :index
+  resources :conclusions, except: :index
+  resources :solutions, except: :index
+  resources :permissions, except: :index
+  resources :ikcontracts, except: :index
+  resources :certcontracts, except: :index
+  resources :inventories, except: :index
+  resources :attachments, except: :index
+  resources :users
 
   get       '/signup',  to: 'users#new'
   get       '/login',   to: 'sessions#new'
   post      '/login',   to: 'sessions#create'
   delete    '/logout',  to: 'sessions#destroy'
-  resources :users
-
-  get 'welcome/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
