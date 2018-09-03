8b945a51d2bf54382144381ef1e11763f7479d81
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-09-03 04:57:17 +0300

    refactoring proposals

---
+++ config/initializers/constants.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+TYPE_OWNERSHIP = %w[государственная негосударственная муниципальная смешанная частная].freeze
+TYPE_PRODUCTION = %w[серийный\ выпуск партия единичное\ изделие].freeze
+CERTIFICATION_SCHEME = %w[3c 4c 6c].freeze
+COMPLIXITY_GROUP = %w[1 2 3].freeze
