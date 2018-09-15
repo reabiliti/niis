4b58a324556f656ba2291d9860ccb53120c3a334
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-09-16 02:59:48 +0300

    refactoring certcontracts (#34)

--- app/models/certcontract.rb
+++ app/models/certcontract.rb
@@ -5,7 +5,4 @@ class Certcontract < ApplicationRecord
   belongs_to :proposal
   validates :proposal_id, uniqueness: true
   validates :cecon_registration_num, :cecon_registration_date, :cecon_date_from, :cecon_date_expiry, presence: true
-
-  DECLENSION_NAME = %w[именуемый именуемая именуемое].freeze
-  DECLENSION_ACTIVE = %w[действующего действующей действующий действующее действующая].freeze
 end
