cdef5b594653d31a860d7bc4171c5d4cdf2155bb
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-09-15 19:04:09 +0300

    refactoring inventories

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class InventoriesController < ApplicationController
-  before_action :inv_find, only: %i[show edit update destroy]
+  before_action :inventory_find, only: %i[show edit update destroy]
   before_action :setting_find, only: %i[show new]
-
-  def index; end
 
   def show
     @proposal = Proposal.find(@inventory.proposal_id)
@@ -18,29 +16,39 @@ class InventoriesController < ApplicationController
   end
 
   def create
-    @inventory = Inventory.new(inv_params)
-    @inventory.save ? (redirect_to @inventory) : (render 'new')
+    @inventory = Inventory.new(inventory_params)
+    if @inventory.save
+      redirect_to @inventory, flash: { success: t('inventories.notices.success.create') }
+    else
+      flash.now[:danger] = t('inventories.notices.danger.create')
+      render :new
+    end
   end
 
   def edit; end
 
   def update
-    @inventory.update(inv_params) ? (redirect_to @inventory) : (render 'new')
+    if @inventory.update(inventory_params)
+      redirect_to @inventory, flash: { success: t('inventories.notices.success.update') }
+    else
+      flash.now[:danger] = t('inventories.notices.danger.update')
+      render :new
+    end
   end
 
   def destroy
     @inventory.destroy
-    redirect_to root_path
+    redirect_to root_path, flash: { success: t('inventories.notices.success.destroy') }
   end
 
   private
 
-  def inv_params
+  def inventory_params
     params.require(:inventory).permit(:proposal_id, :inv_date_from, :inv_list_doc,
                                       :inv_list_page, :inv_exec_name, :inv_chief_name)
   end
 
-  def inv_find
+  def inventory_find
     @inventory = Inventory.find(params[:id])
   end
 
