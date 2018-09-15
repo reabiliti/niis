12c19865f82b977b14aed76388abc8883a67100b
Aleksandr Dyuzhikov <alex.duzh@gmail.com>
2018-09-15 06:11:57 +0300

    refactoring certificates

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -6,9 +6,7 @@ class Certificate < ApplicationRecord
   validates :cert_blank_num, uniqueness: true
   validates :cert_expiry_date, :cert_registration_date, :cert_registration_num,
             :cert_blank_num, :cert_applic_name, :cert_manuf_name, presence: true
-  has_one :permission, dependent: :destroy, inverse_of: :certificate
-  has_one :ikcontract, dependent: :destroy, inverse_of: :certificate
-  has_many :attachments, dependent: :destroy, inverse_of: :certificate
-
-  FONT_SIZE = %w[10 11 12].freeze
+  has_one :permission, dependent: :destroy
+  has_one :ikcontract, dependent: :destroy
+  has_many :attachments, dependent: :destroy
 end
